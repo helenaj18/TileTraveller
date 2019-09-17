e37f244c031a61f642fedb5af9acba6360a40c1d
Gunnhildur katrín Erlendsdóttir <GunnhildurKatrin@Gunnhildurs-MacBook-Air.local>
2019-09-17 10:01:58 +0000

    second function made

--- tile_traveller.py
+++ tile_traveller.py
@@ -10,6 +10,7 @@ initial_location = 1.1
 
 location = initial_location
 
+
 def travel_options(location):
     N='(N)orth'
     S='(S)outh'
@@ -28,6 +29,49 @@ def travel_options(location):
         print('You can travel: ', E,'or',W)
     elif location==3.2:
         print('You can travel: ', N,'or', S)
+
+
+def input_direction(location):
+    direction = input('Direction: ')
+    direction=direction.lower()
+
+    while location != 3.1:
+
+        if location == 1.1 and direction == 'n':
+            location = 1.2
+        elif location == 1.2 and direction == 'n':
+            location = 1.3
+        elif location == 1.2 and direction == 's':
+            location = 1.1
+        elif location == 1.2 and direction == 'e':
+            location = 2.2
+        elif location == 1.3 and direction == 'e':
+            location = 2.3
+        elif location == 1.3 and direction == 's':
+            location = 1.2
+        elif location == 2.3 and diretion == 'e':
+            location = 3.3
+        elif location == 2.3 and direction == 'w':
+            location = 1.3
+        elif location == 3.3 and direction == 's':
+            location = 3.2
+        elif location == 3.3 and direction == 'w':
+            location = 2.3
+        elif location == 3.2 and direction == 'n':
+            location = 3.3
+        elif location == 3.2 and direction == 's':
+            location = 3.1
+        elif location == 2.2 and direction == 's':
+            location = 2.1
+        elif location == 2.2 and direction == 'w':
+            location = 1.2
+        elif location == 2.1 and direction == 'n':
+            location = 2.2
+        else:
+            print('Not a valid direction!')
+
+        direction = input('Direction: ')
+        direction = direction.lower()
 
 
 
