376399d49aea8c8f70c7ecdc092686a13dc83a02
Helena Jónsdóttir <helenaj18@ru.is>
2019-09-17 09:15:48 +0000

    Algorithm made

---
+++ tile_traveller.py
@@ -0,0 +1,6 @@
+#Starting location is (1,1)
+#Print travel options for the location
+#Write possible travel direction
+#Ask the user for direction
+#If the user inputs a invalid direction, print "Not a valid direction!" and try again
+#Ask user for direction until they are at (3,1), then print "Victory!"
