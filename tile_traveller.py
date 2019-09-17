90063420736d7097379713dde8079d57195c29f6
Helena Jónsdóttir <helenaj18@ru.is>
2019-09-17 09:46:23 +0000

    Function defined

--- tile_traveller.py
+++ tile_traveller.py
@@ -10,6 +10,27 @@ initial_location = 1.1
 
 location = initial_location
 
+def travel_options(location):
+    N='(N)orth'
+    S='(S)outh'
+    E='(E)ast'
+    W='(W)est'
+
+    if location==1.1 or location==2.1 or location==3.1:
+        print('You can travel: ', N)
+    elif location==1.2:
+        print('You can travel: ', N,'or',S,'or',E)
+    elif location==1.3:
+        print('You can travel: ', S, 'or', E)
+    elif location==2.2 or location==3.3:
+        print('You can travel: ', S, 'or', W)
+    elif location==2.3:
+        print('You can travel: ', E,'or',W)
+    elif location==3.2:
+        print('You can travel: ', N,'or', S)
+
+
+
 while location!=3.1:
     travel_options(location)
 
