080450ccab11b1d1a81e5f10ee9db36eadfddf34
Helena Jónsdóttir <helenaj18@ru.is>
2019-09-17 10:24:23 +0000

    Almost done

--- tile_traveller.py
+++ tile_traveller.py
@@ -18,68 +18,63 @@ def travel_options(location):
     W='(W)est'
 
     if location==1.1 or location==2.1 or location==3.1:
-        print('You can travel: ', N)
+        print(f'You can travel: {N}.')
     elif location==1.2:
-        print('You can travel: ', N,'or',S,'or',E)
+        print(f'You can travel: {N} or {E} or {S}.')
     elif location==1.3:
-        print('You can travel: ', S, 'or', E)
+        print(f'You can travel: {E} or {S}.')
     elif location==2.2 or location==3.3:
-        print('You can travel: ', S, 'or', W)
+        print(f'You can travel: {S} or {W}.')
     elif location==2.3:
-        print('You can travel: ', E,'or',W)
+        print(f'You can travel: {E} or {W}.')
     elif location==3.2:
-        print('You can travel: ', N,'or', S)
+        print(f'You can travel: {N} or {S}.')
+    return location
 
 
 def input_direction(location):
     direction = input('Direction: ')
     direction=direction.lower()
 
-    while location != 3.1:
-
-        if location == 1.1 and direction == 'n':
-            location = 1.2
-        elif location == 1.2 and direction == 'n':
-            location = 1.3
-        elif location == 1.2 and direction == 's':
-            location = 1.1
-        elif location == 1.2 and direction == 'e':
-            location = 2.2
-        elif location == 1.3 and direction == 'e':
-            location = 2.3
-        elif location == 1.3 and direction == 's':
-            location = 1.2
-        elif location == 2.3 and diretion == 'e':
-            location = 3.3
-        elif location == 2.3 and direction == 'w':
-            location = 1.3
-        elif location == 3.3 and direction == 's':
-            location = 3.2
-        elif location == 3.3 and direction == 'w':
-            location = 2.3
-        elif location == 3.2 and direction == 'n':
-            location = 3.3
-        elif location == 3.2 and direction == 's':
-            location = 3.1
-        elif location == 2.2 and direction == 's':
-            location = 2.1
-        elif location == 2.2 and direction == 'w':
-            location = 1.2
-        elif location == 2.1 and direction == 'n':
-            location = 2.2
-        else:
-            print('Not a valid direction!')
-
-        direction = input('Direction: ')
-        direction = direction.lower()
-
+    if location == 1.1 and direction == 'n':
+        location = 1.2
+    elif location == 1.2 and direction == 'n':
+        location = 1.3
+    elif location == 1.2 and direction == 's':
+        location = 1.1
+    elif location == 1.2 and direction == 'e':
+        location = 2.2
+    elif location == 1.3 and direction == 'e':
+        location = 2.3
+    elif location == 1.3 and direction == 's':
+        location = 1.2
+    elif location == 2.3 and direction == 'e':
+        location = 3.3
+    elif location == 2.3 and direction == 'w':
+        location = 1.3
+    elif location == 3.3 and direction == 's':
+        location = 3.2
+    elif location == 3.3 and direction == 'w':
+        location = 2.3
+    elif location == 3.2 and direction == 'n':
+        location = 3.3
+    elif location == 3.2 and direction == 's':
+        location = 3.1
+    elif location == 2.2 and direction == 's':
+        location = 2.1
+    elif location == 2.2 and direction == 'w':
+        location = 1.2
+    elif location == 2.1 and direction == 'n':
+        location = 2.2
+    else:
+        print('Not a valid direction!')
+    return location
 
 
 while location!=3.1:
     travel_options(location)
 
-    input_direction(location)
+    location=input_direction(location)
 
-    change_location(location)
 else:
     print('Victory!')
